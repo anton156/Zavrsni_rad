cebe8c1af3bbd53cca0a53ac2e827f1e70c28777
anton156 <aviskic@gmail.com>
2020-07-29 00:10:49 +0200

    pdf fix

--- resources/views/vendor/datatables/print.blade.php
+++ resources/views/vendor/datatables/print.blade.php
@@ -1,17 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <title>Print Table</title>
-        <meta charset="UTF-8">
-        <meta name=description content="">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" rel="stylesheet">
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
-        <!-- Bootstrap CSS -->
-        <style>
-            body {margin: 20px}
-        </style>
-    </head>
+@extends('layouts.pdf')
+
+@section('content')
     <body>
         <table class="table table-bordered table-condensed table-striped">
             @foreach($data as $row)
@@ -34,4 +23,4 @@
             @endforeach
         </table>
     </body>
-</html>
+@endsection
